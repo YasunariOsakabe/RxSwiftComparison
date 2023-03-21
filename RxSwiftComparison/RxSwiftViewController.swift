0dc900eefd44963c09b7a8d565f72d2f343ec296
YasunariOsakabe <yasunari0296@gmail.com>
2023-03-21 14:27:28 +0900

    RxSwift.verを更新

--- RxSwiftComparison/RxSwiftViewController.swift
+++ RxSwiftComparison/RxSwiftViewController.swift
@@ -13,14 +13,21 @@ class RxSwiftViewController: UIViewController {
     @IBOutlet weak var outputLabel: UILabel!
     @IBOutlet weak var inputTextField: UITextField!
     
+    private let maxLength: Int = 10
     private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        inputTextField.rx.text
-            .bind(to: outputLabel.rx.text)
-            .disposed(by: disposeBag)
+        
+        inputTextField.rx.text.subscribe(onNext: { text in
+            if let text = text, text.count >= self.maxLength {
+                self.inputTextField.text = text.prefix(self.maxLength).description
+            }
+            //inputTextFieldの値がoutputLabelに反映されるように設定
+            self.outputLabel.text = self.inputTextField.text
+        })
+        //購読解除
+        .disposed(by: disposeBag)
     }
 
 }
