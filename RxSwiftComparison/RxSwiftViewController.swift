f7ecdba3454c869debf51dc99d959e1ff0158e83
YasunariOsakabe <yasunari0296@gmail.com>
2023-03-21 16:10:17 +0900

    RxSwift.verを更新

--- RxSwiftComparison/RxSwiftViewController.swift
+++ RxSwiftComparison/RxSwiftViewController.swift
@@ -9,25 +9,54 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-class RxSwiftViewController: UIViewController {
+class RxViewController: UIViewController {
     @IBOutlet weak var outputLabel: UILabel!
     @IBOutlet weak var inputTextField: UITextField!
-    
+    @IBOutlet weak var remainTextCountLabel: UILabel!
+    @IBOutlet weak var button: UIButton!
+
     private let maxLength: Int = 10
+    private let minimumTextLength: Int = 6
+    private let normalTextColor: UIColor = .black
+    private let limitedTextColor: UIColor = .red
     private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        button.setBackgroundImage(UIImage(ciImage: .gray), for: .disabled)
+
+        // 入力内容変更時
         inputTextField.rx.text.subscribe(onNext: { text in
+            // 文字列長によってテキストカラーを変える
             if let text = text, text.count >= self.maxLength {
                 self.inputTextField.text = text.prefix(self.maxLength).description
+                self.inputTextField.textColor = self.limitedTextColor
+                print("入力文字数上限のため文字色を赤に変更します。")
+            } else {
+                self.inputTextField.textColor = self.normalTextColor
             }
-            //inputTextFieldの値がoutputLabelに反映されるように設定
+            // ラベルに入力された文字列を反映する
             self.outputLabel.text = self.inputTextField.text
-        })
-        //購読解除
-        .disposed(by: disposeBag)
+
+            // 入力されている文字数を取得
+            let inputTextLength = self.inputTextField.text?.count ?? 0
+
+            // あと何文字入力できるかを表示する
+            let remainCount = self.maxLength - inputTextLength
+            self.remainTextCountLabel.text = "あと\(remainCount)文字入力できます"
+
+            // ボタンの押下制御
+            self.button.isEnabled = (inputTextLength >= self.minimumTextLength)
+        }).disposed(by: disposeBag)
+
+        // ボタンタップ時
+        button.rx.tap.subscribe(onNext: {
+            if let nextViewController = UIStoryboard(name: "NextViewController", bundle: nil)
+                .instantiateInitialViewController() as? NextViewController {
+                self.navigationController?.pushViewController(nextViewController, animated: true)
+            }
+        }).disposed(by: disposeBag)
     }
 
 }
